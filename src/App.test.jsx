89ea5ff9cec2daba45fb3969dcbbab22c6a42732
kevinkim <kevinnkimm96@gmail.com>
2023-03-08 22:47:16 +0800

    add test for app

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test App", () => {
+	test("renders page title", () => {
+		render(<App />);
+		const title = screen.getByText(/little lemon restaurant/i);
+		expect(title).toBeInTheDocument();
+	});
+
+	test("renders view menu button", () => {
+		render(<App />);
+		const menuBtn = screen.getByRole("button", {
+			name: /view menu/i,
+		});
+		expect(menuBtn).toBeInTheDocument();
+	});
+
+	test("renders book table button", () => {
+		render(<App />);
+		const menuBtn = screen.getByRole("button", {
+			name: /reserve a table/i,
+		});
+		expect(menuBtn).toBeInTheDocument();
+	});
 });
