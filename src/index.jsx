08220b0f6f5dcb31db19d9e3a233e2a6243390a0
kevinkim <kevinnkimm96@gmail.com>
2023-03-08 22:41:08 +0800

    add antd config and context provider

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
+import { FormProvider } from "./context/FormContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<ConfigProvider
+			theme={{
+				token: { colorPrimary: "#495E57" },
+			}}
+		>
+			<FormProvider>
+				<App />
+			</FormProvider>
+		</ConfigProvider>
 	</React.StrictMode>,
 );
 
