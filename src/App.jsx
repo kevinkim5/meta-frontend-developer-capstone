b8a1e5253240365e40cf1148d509a0b6992c63d1
kevinkim <kevinnkimm96@gmail.com>
2023-03-08 22:46:53 +0800

    add app file to handle routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useFormContext } from "./context/FormContext";
+
+import "./App.css";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+
+import Booking from "./screens/Booking";
+import BookingConfirmation from "./screens/BookingConfirmation";
+import Home from "./screens/Home";
+
+export default function App() {
+	const { formData } = useFormContext();
+
+	return (
+		<BrowserRouter>
+			<Nav />
+			<div className="main-page">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/reservations" element={<Booking />} />
+					<Route
+						path="/reservation-confirmation"
+						element={<BookingConfirmation {...formData} />}
+					/>
+				</Routes>
+			</div>
+			<Footer />
+		</BrowserRouter>
+	);
 }
-
-export default App;
